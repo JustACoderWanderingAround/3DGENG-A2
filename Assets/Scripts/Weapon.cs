0d10985a386b7fb53939cef6ae8dc59ba6aa96d2
JustACoderWanderingAround <pgbaquiran@gmail.com>
2023-10-30 17:44:41 +0800

    added weapons, added base gun abstract class

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Weapon
+{
+    private int maxBullets;
+    private int currentBullets;
+
+    public int MaxBullets   // property
+    {
+        get { return maxBullets; }   // get method
+        set { maxBullets = value; }  // set method
+    }
+
+    public int CurrentBullets   // property
+    {
+        get { return currentBullets; }   // get method
+        set { currentBullets = value; }  // set method
+    }
+
+    public abstract void Shoot();
+    
+    public virtual void Reload()
+    {
+        currentBullets = maxBullets;
+        // TODO: To be overriden with more functionalities
+    }
+}
+
