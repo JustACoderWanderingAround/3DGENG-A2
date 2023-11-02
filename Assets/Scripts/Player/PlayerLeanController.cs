7bf5e15ec6f8f3a0f291a7a8007fdbdb0b2aad36
JustACoderWanderingAround <pgbaquiran@gmail.com>
2023-11-02 23:27:27 +0800

    ADDED LEAN SPRITE + COUGH SYRUP FTW

---
+++ Assets/Scripts/Player/PlayerLeanController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLeanController : MonoBehaviour
+{
+    public Animator animator;
+    public LayerMask layers;
+    public RaycastHit Hit;
+
+    void Update()
+    {
+        if (Input.GetAxis("LeanL") > 0)
+        {
+            //if (!Physics.Raycast(transform.position, -transform.right, out Hit, 1f, layers))
+            //{
+                animator.ResetTrigger("Idle");
+                animator.ResetTrigger("Right");
+                animator.SetTrigger("Left");
+            //}
+        }
+        else if (Input.GetAxis("LeanR") > 0)
+        {
+            //if (!Physics.Raycast(transform.position, transform.right, out Hit, 1f, layers))
+            //{
+                animator.ResetTrigger("Idle");
+                animator.ResetTrigger("Left");
+                animator.SetTrigger("Right");
+            //}
+        }
+        else
+        {
+            animator.ResetTrigger("Right");
+            animator.ResetTrigger("Left");
+            animator.SetTrigger("Idle");
+        }
+    }
+}
