3c216486a79d2c41560ef14092d730b3fb44664b
JustACoderWanderingAround <pgbaquiran@gmail.com>
2023-11-19 19:22:24 +0800

    YOOOOO FIRING MODES WORK I LOVE MY LIFE FUCK YEAH

--- Assets/Scripts/ScriptableObject/ShootConfigScriptableObject.cs
+++ Assets/Scripts/ScriptableObject/ShootConfigScriptableObject.cs
@@ -10,6 +10,8 @@ public class ShootConfigScriptableObject : ScriptableObject
     public bool isHitscan = true;
     // Fire rate of the gun
     public float fireRate;
+    // Number of rounds in a burst
+    public int burstNum;
 
     public float maxSpreadTime;
 
@@ -19,67 +21,7 @@ public class ShootConfigScriptableObject : ScriptableObject
 
     public int damage;
 
-    ///
-    /// This enum determines how the recoil reacts - will it go straight up? straight down?
-    /// left and right? 
-    /// up then down? right then left?
-    ///
-    //public enum BehaviourType
-    //{
-    //    LINEAR,
-    //    SINE,
-    //    QUADRATIC
-    //}
+    public bool canAuto;
 
-    //public BehaviourType horiRecoilBehaviour;
-    //public BehaviourType vertRecoilBehaviour;
-
-    // Max offset from forward when firing
-
-
-    /// <summary>
-    /// All offsets from the forward direction are a function of time.
-    /// This function calculates the offset on each axis based on a specific formulae.
-    /// </summary>
-    /// <param name="shootTime">The amount of time the player has held the trigger.</param>
-    //public Vector3 GenerateSpreadAtPoint(float shootTime)
-    //{
-    //    Vector3 spread = Vector3.zero;
-
-    //    /// psuedocode:
-    //    /// for each axis:
-    //    ///     if behaviourType is linear
-    //    ///         lerp from 0 to maxRecoilOffset.axis
-    //    ///     else if behaviourType is sine
-    //    ///         lerp from 0 to maxRecoilOffset.axis
-    //    ///         apply sine equation to result
-    //    ///     else if behaviourType is quadratic
-    //    ///         apply quadratic equation to result
-    //    ///         -> SLERP         
-    //    switch (horiRecoilBehaviour) {
-    //        case BehaviourType.LINEAR:
-    //            spread.x = (Vector3.Lerp(Vector3.zero, cameraShakeStrength, Mathf.Clamp(shootTime / maxSpreadTime, 0f, 1f))).x;
-    //            break;
-    //        case BehaviourType.SINE:
-    //            spread.x = Mathf.Sin(shootTime * cameraShakeStrength.x);
-    //            break;
-    //        case BehaviourType.QUADRATIC:
-    //            spread.x = (Vector3.Slerp(Vector2.zero, cameraShakeStrength, Mathf.Clamp(shootTime / maxSpreadTime, 0f, 1f))).x;
-    //            break;
-    //    }
-    //    switch (vertRecoilBehaviour)
-    //    {
-    //        case BehaviourType.LINEAR:
-    //            spread.y = (Vector3.Lerp(Vector3.zero, cameraShakeStrength, Mathf.Clamp(shootTime / maxSpreadTime, 0f, 1f))).y;
-    //            break;
-    //        case BehaviourType.SINE:
-    //            spread.y = Mathf.Sin(shootTime * cameraShakeStrength.y);
-    //            break;
-    //        case BehaviourType.QUADRATIC:
-    //            spread.y = (Vector3.Slerp(Vector2.zero, cameraShakeStrength, Mathf.Clamp(shootTime / maxSpreadTime, 0f, 1f))).y;
-    //            break;
-    //    }
-
-    //    return spread;
-    //}
+    public bool canBurst;
 }
--- Assets/Scripts/Weapons/Gun.cs
+++ Assets/Scripts/Weapons/Gun.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class Gun : Weapon
 {
+    public enum FireMode
+    {
+        MODE_SEMIAUTO = 0,
+        MODE_AUTO = 1,
+        MODE_BURST = 2,
+        NUM_MODES
+    }
+    public FireMode fireMode = FireMode.MODE_SEMIAUTO;
     public BulletConfigurationScriptableObject currBullet;
 
     float shootTimer, maxShootTimer, triggerHoldTimer;
 
+    bool canShoot;
 
+    int roundCounter;
+    bool xKeyDown;
     public override string GetClassName()
     {
         return "Gun";
@@ -19,39 +30,137 @@ public class Gun : Weapon
         currentBullets = maxBullets;
         shootTimer = 0;
         maxShootTimer = 60 / shootConfig.fireRate;
+        canShoot = false;
+        roundCounter = shootConfig.burstNum;
+        xKeyDown = false;
     }
     private void Update()
     {
         if (shootTimer > -1)
             shootTimer -= Time.deltaTime;
-        if (Input.GetAxisRaw("Fire1") > 0)
+        if (Input.GetAxisRaw("Fire1") < 1)
         {
-            triggerHoldTimer += Time.deltaTime;
+            canShoot = true;
+            if (roundCounter < shootConfig.burstNum)
+            {
+                roundCounter = shootConfig.burstNum;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.X) && xKeyDown == false)
+        {
+            xKeyDown = true;
+            Debug.Log("swapped");
+            SwapFireMode();
         }
         else
         {
-            triggerHoldTimer = 0;
+           xKeyDown = false;
         }
-
     }
     public override bool Shoot()
     {
-        if (shootTimer < 0 && currentBullets > 0)
+        if (shootTimer < 0 && currentBullets > 0 && canShoot)
         {
-            currentBullets -= 1;
-            shootTimer = maxShootTimer;
-            GameObject newBullet = Instantiate(bulletPrefab, barrelTip.transform.position, Quaternion.identity);
-            newBullet.GetComponent<Bullet>().Init(transform.parent.transform.forward, currBullet.BulletVelocity);
-            return true;
+            switch (fireMode)
+            {
+                case FireMode.MODE_SEMIAUTO:
+                    ShootBullet();
+                    canShoot = false;
+                    return true;
+                case FireMode.MODE_AUTO:
+                    ShootBullet();
+                    return true;
+                case FireMode.MODE_BURST:
+                    if (roundCounter > 0)
+                    {
+                        ShootBullet();
+                        roundCounter -= 1;
+                        if (roundCounter < 1)
+                        {
+                            canShoot = false;
+                        }
+                        return true;
+                    }
+                    else
+                        return false;
+                default:
+                    return false;
+            }
         }
         else
             return false;
-
     }
     public override void Reload()
     {
-       
+
         base.Reload();
-        
+
+    }
+    private void ShootBullet()
+    {
+        currentBullets -= 1;
+        shootTimer = maxShootTimer;
+        GameObject newBullet = Instantiate(bulletPrefab, barrelTip.transform.position, Quaternion.identity);
+        newBullet.GetComponent<Bullet>().Init(transform.parent.transform.forward, currBullet.BulletVelocity);
+    }
+    //private void SwapFireMode() 
+    //{
+    //    if (fireMode == FireMode.MODE_SEMIAUTO)
+    //    {
+    //        if (shootConfig.canAuto)
+    //        {
+    //            fireMode = FireMode.MODE_AUTO;
+    //        }
+    //        else if (shootConfig.canBurst)
+    //        {
+    //            fireMode = FireMode.MODE_BURST;
+    //        }
+    //    }
+    //    else if (fireMode == FireMode.MODE_AUTO)
+    //    {
+    //        if (shootConfig.canBurst)
+    //        {
+    //            fireMode = FireMode.MODE_BURST;
+    //        }
+    //        else
+    //        {
+    //            fireMode = FireMode.MODE_SEMIAUTO;
+    //        }
+    //    }
+    //    else if (fireMode == FireMode.MODE_BURST)
+    //    {
+    //        fireMode = FireMode.MODE_SEMIAUTO;
+    //    }
+    //}
+    private void SwapFireMode()
+    {
+        if (!shootConfig.canBurst && !shootConfig.canAuto)
+        {
+            return;
+        }
+        switch (fireMode)
+        {
+            case FireMode.MODE_SEMIAUTO:
+                if (shootConfig.canAuto)
+                {
+                    fireMode = FireMode.MODE_AUTO;
+                    Debug.Log("auto");
+                }
+                else if (shootConfig.canBurst)
+                {
+                    fireMode = FireMode.MODE_BURST;
+                    Debug.Log("burst");
+                }
+                break;
+
+            case FireMode.MODE_AUTO:
+                fireMode = shootConfig.canBurst ? FireMode.MODE_BURST : FireMode.MODE_SEMIAUTO;
+                break;
+
+            case FireMode.MODE_BURST:
+                fireMode = FireMode.MODE_SEMIAUTO;
+                Debug.Log("auto");
+                break;
+        }
     }
 }
