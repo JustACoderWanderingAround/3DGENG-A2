cab052cdb8fdba68378148d256d3f723e3f4bafe
JustACoderWanderingAround <pgbaquiran@gmail.com>
2023-12-03 15:07:38 +0800

    crouching done !!

--- Assets/Scripts/Player/Movement/PlayerCrouchProneController.cs
+++ Assets/Scripts/Player/Movement/PlayerCrouchProneController.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class PlayerCrouchProneController : MonoBehaviour
 {
     PlayerMovementController movementController;
+
+    private Vector3 defaultScale = new Vector3(1, 1, 1);
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,18 @@ public class PlayerCrouchProneController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        switch(movementController.stanceState)
+        {
+            case PlayerMovementController.StanceState.State_Sprint:
+            case PlayerMovementController.StanceState.State_Stand:
+                gameObject.transform.localScale = defaultScale;
+                break;
+            case PlayerMovementController.StanceState.State_Crouch:
+                gameObject.transform.localScale = new Vector3(1, 0.65f, 1);
+                break;
+            case PlayerMovementController.StanceState.State_Prone:
+                gameObject.transform.localScale = new Vector3(1, 0.2f, 1);
+                break;
+        }
     }
 }
